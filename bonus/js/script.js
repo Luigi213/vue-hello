382ac5e4a1d0687bb3139cfe7099db1c390c3d6d
Luigi213 <reyesgianluigi88@gmail.com>
2022-12-16 13:22:05 +0100

    bonus done

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -4,6 +4,7 @@ createApp({
     data() {
         return {
             message: 'Ciao Vue',
+            photo: 'https://i.picsum.photos/id/2/300/300.jpg?hmac=18v9hYebAqcHuhqDo_PgGIHxZ5Ay-8Di7TqUUS5ZvXY'
         }
     }
 }).mount('#app')
